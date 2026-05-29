54ee865a855802409f1cc23d0aa688aa71105ae6
ShikharSingh7985 <s4shikharsingh@gmail.com>
2026-05-29 10:30:39 +0530

    some changes

--- ChatModels/chatmodel_google.py
+++ ChatModels/chatmodel_google.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-models= ChatGoogleGenerativeAI(model="gemini-3.5-flash")
+models= ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 result=models.invoke("What is the capital of india?")
 
 # Access the text element in the first block
-print(result.content[0]['text'])
+print(result.content)
