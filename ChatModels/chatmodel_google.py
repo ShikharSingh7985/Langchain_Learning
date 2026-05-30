c6f9277e42ee351f0d05567e5f99ad7a6e7f5b89
ShikharSingh7985 <s4shikharsingh@gmail.com>
2026-05-31 01:33:12 +0530

    prompt template

--- ChatModels/chatmodel_google.py
+++ ChatModels/chatmodel_google.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 models= ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-result=models.invoke("What is the capital of india?")
+result=models.invoke("give me code of leetcode problem 25 in cpp only fill the function reversekgroup")
 
 # Access the text element in the first block
 print(result.content)
